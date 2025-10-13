c96c33885c93bd8082817d41c63a36263371a202
Dave <uchennadavid2404@gmail.com>
2025-10-13 15:45:22 +0100

    fix again

--- src/modules/wishlist/models/2025_10_14_alter_wishlist_items_table.ts
+++ src/modules/wishlist/models/2025_10_14_alter_wishlist_items_table.ts
@@ -1,17 +1,5 @@
 import { Knex } from 'knex';
 
-// export async function up(knex: Knex): Promise<void> {
-// 	await knex.schema.alterTable('wishlist_items', (table) => {
-// 		table.string('uniqueLink', 255).notNullable().unique().alter();
-// 	});
-// }
-
-// export async function down(knex: Knex): Promise<void> {
-// 	await knex.schema.alterTable('wishlist_items', (table) => {
-// 		table.string('uniqueLink', 50).notNullable().unique().alter();
-// 	});
-// }
-
 export async function up(knex: Knex) {
 	const hasColumn = await knex.schema.hasColumn('wishlist_items', 'uniqueLink');
 	if (!hasColumn) {
