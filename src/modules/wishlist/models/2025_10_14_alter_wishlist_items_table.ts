f262d24abbbba38ce7cc220bc4b8455de440919a
Dave <uchennadavid2404@gmail.com>
2025-10-13 15:44:59 +0100

    fix

--- src/modules/wishlist/models/2025_10_14_alter_wishlist_items_table.ts
+++ src/modules/wishlist/models/2025_10_14_alter_wishlist_items_table.ts
@@ -1,13 +1,29 @@
 import { Knex } from 'knex';
 
-export async function up(knex: Knex): Promise<void> {
-	await knex.schema.alterTable('wishlist_items', (table) => {
-		table.string('uniqueLink', 255).notNullable().unique().alter();
-	});
+// export async function up(knex: Knex): Promise<void> {
+// 	await knex.schema.alterTable('wishlist_items', (table) => {
+// 		table.string('uniqueLink', 255).notNullable().unique().alter();
+// 	});
+// }
+
+// export async function down(knex: Knex): Promise<void> {
+// 	await knex.schema.alterTable('wishlist_items', (table) => {
+// 		table.string('uniqueLink', 50).notNullable().unique().alter();
+// 	});
+// }
+
+export async function up(knex: Knex) {
+	const hasColumn = await knex.schema.hasColumn('wishlist_items', 'uniqueLink');
+	if (!hasColumn) {
+		await knex.schema.alterTable('wishlist_items', (table) => {
+			table.string('uniqueLink', 255).unique();
+		});
+	}
 }
 
-export async function down(knex: Knex): Promise<void> {
+export async function down(knex: Knex) {
 	await knex.schema.alterTable('wishlist_items', (table) => {
-		table.string('uniqueLink', 50).notNullable().unique().alter();
+		table.dropUnique(['uniqueLink']);
+		table.dropColumn('uniqueLink');
 	});
 }
